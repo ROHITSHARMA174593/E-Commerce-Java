c1ddb0ca2d8d499a445f8f6db6f239e913d9f0f0
Rohit Sharma <your_actual_email@example.com>
2025-11-22 23:32:44 +0530

    Add More Models in EcomSB Project Like Role, User, Address etc

--- EcomSB/src/main/java/com/ecom/EcomSB/model/Product.java
+++ EcomSB/src/main/java/com/ecom/EcomSB/model/Product.java
@@ -7,24 +7,27 @@ import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import lombok.ToString;
 
 @Entity
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
+@Table(name = "products")
+@ToString
 public class Product {
+
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long productId;
 
     @NotBlank
-    @Size(min = 3, message = "Product Name must contain atleast 3 characters")
+    @Size(min = 3, message = "Product name must contain atleast 3 characters")
     private String productName;
-
     private String image;
 
     @NotBlank
-    @Size(min = 3, message = "Product Description must contain atleast 6 characters")
+    @Size(min = 6, message = "Product description must contain atleast 6 characters")
     private String description;
     private Integer quantity;
     private double price;
@@ -35,4 +38,7 @@ public class Product {
     @JoinColumn(name = "category_id")
     private Category category;
 
+    @ManyToOne
+    @JoinColumn(name = "seller_id")
+    private User user;
 }
