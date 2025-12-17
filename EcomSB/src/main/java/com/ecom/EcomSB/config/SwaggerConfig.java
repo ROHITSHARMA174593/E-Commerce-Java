98382fc501ecd6de2fd92f9cb7dab3f9c1dc9c8f
Rohit Sharma <your_actual_email@example.com>
2025-12-17 23:31:25 +0530

    Final Push for Backend! Backend is Totally Complete with Swagger API Documentation

--- EcomSB/src/main/java/com/ecom/EcomSB/config/SwaggerConfig.java
+++ EcomSB/src/main/java/com/ecom/EcomSB/config/SwaggerConfig.java
@@ -1,7 +1,11 @@
 package com.ecom.EcomSB.config;
 
 import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.ExternalDocumentation;
 import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Contact;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
 import io.swagger.v3.oas.models.security.SecurityRequirement;
 import io.swagger.v3.oas.models.security.SecurityScheme;
 import org.springframework.context.annotation.Bean;
@@ -25,6 +29,15 @@ public class SwaggerConfig {
                 .addList("bearerAuth");
 
         return new OpenAPI()
+                .info(new Info()
+                        .title("Sprint Boot E-Commerce API")
+                        .version("1.0")
+                        .description("This is a Sprint-Boot Project for E-Commerce")
+                        .license(new License().name("Apache 2.0").url("http://iaddurlafterbuilditcompletely"))
+                        .contact(new Contact().name("Rohit Sharma").email("rohitsharma45055@gmail.com").url("https://github.com/ROHITSHARMA174593"))
+                )
+                .externalDocs(new ExternalDocumentation()
+                        .description("Project Documentation"))
                 .components(new Components()
                         .addSecuritySchemes("bearerAuth", bearerScheme))
                 .addSecurityItem(bearerRequirement);
--- EcomSB/src/main/java/com/ecom/EcomSB/payload/CategoryDTO.java
+++ EcomSB/src/main/java/com/ecom/EcomSB/payload/CategoryDTO.java
@@ -1,6 +1,7 @@
 package com.ecom.EcomSB.payload;
 
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -9,7 +10,9 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @AllArgsConstructor
 public class CategoryDTO {
+    @Schema(description = "Category ID")
     private Long categoryId;
+    @Schema(description = "Category name for category you wish to create")
     private String categoryName;
 
 
